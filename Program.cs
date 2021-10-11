5358b317409c7a3ef2ba4b751e22a386ae34051b
JGouyau <Gouyau.jimmy@gmail.com>
2021-10-11 11:10:09 +0200

    fini

--- Program.cs
+++ Program.cs
@@ -1,15 +1,31 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Json
 {
     class Program
     {
+        
+
         static void Main(string[] args)
         {
+            string path = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName;
+            string filename = File.ReadAllText( path + "\\weather.json");
+            WeatherInfo weatherinfo = JsonConvert.DeserializeObject<WeatherInfo>(filename);
+
+            Console.WriteLine("test description visibility : " + weatherinfo.visibility);
+            Console.WriteLine("test info main : " + weatherinfo.main["temp"]);
+            Console.WriteLine("test description méteo : "+ weatherinfo.weather[0].description);
+            Console.WriteLine("Deg du vent : " + weatherinfo.wind.deg);
+
+            Console.ReadLine();
         }
     }
+
+
 }
---
+++ WeatherInfo.cs
@@ -0,0 +1,72 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Json
+{
+    class WeatherInfo
+    {
+        public coord coord;
+        public List<Weather> weather;
+        [JsonProperty("base")]
+        public string baSe;
+        public Dictionary<String, double> main;
+        public int visibility;
+        public Wind wind;
+        public Dictionary<String, double> clouds;
+        public int dt;
+        public Sys sys;
+        public int id;
+        public string name;
+        public int cod;
+
+
+
+    }
+
+    class coord
+    {
+        public double lon;
+        public double lat;
+    }
+
+    //class main
+    //{
+    //    public int temp { get; set; }
+    //    public int pressure { get; set; }
+    //    public int humidity { get; set; }
+    //    public int temp_min { get; set; }
+    //    public int temp_max { get; set; }
+
+    //    public main()
+    //    { 
+    //    }
+    //}
+    class Weather
+    {
+        public int id { get; set; }
+        public string main { get; set; }
+        public string description { get; set; }
+        public string icon { get; set; }
+
+    }
+
+    class Wind
+    {
+        public double speed;
+        public int deg;
+    }
+
+    class Sys
+    {
+        public int type;
+        public int id;
+        public double message;
+        public string country;
+        public int sunrise;
+        public int sunset;
+    }
+}
